2b89a7d9f00e018479d2c6280414c001f92bda8c
Ryan Dalby <dalbyryan3@gmail.com>
2022-10-01 19:54:17 -0600

    Added basic ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/object-detection">Object Detection</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className='App-body'>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/object-detection" element={<ObjectDetection/>}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div className="Home">
+        <header className="Home-header">
+            <h1>
+                React ML Test App
+            </h1>
+            <p>
+                This is a React ML test app, a front-end connecting to various machine learning models and other data analysis backends that are expose through a REST API.
+            </p>
+        </header>
     </div>
   );
 }
 
-export default App;
+function ObjectDetection() {
+  return (
+    <div className="ObjectDetection">
+      <header className="ObjectDetection-header">
+          <h1>
+              Object Detection
+          </h1>
+          <p>
+              Detect stuff
+          </p>
+      </header>
+    </div>
+  );
+}
