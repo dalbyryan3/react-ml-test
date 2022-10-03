e279876f010c3f9531d8e6150688857e7a0d19be
Ryan Dalby <dalbyryan3@gmail.com>
2022-10-02 20:38:08 -0600

    Added image upload component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import ImageUploading from 'react-images-uploading';
+
 
 export default function App() {
   return (
@@ -48,16 +50,75 @@ function Home() {
 }
 
 function ObjectDetection() {
+  const maxNumber = 1;
+  const [images, setImages] = React.useState([]);
+  const onChange = (imageList, addUpdateIdx) => {
+    console.log(imageList, addUpdateIdx);
+    setImages(imageList)
+  };
+
   return (
     <div className="ObjectDetection">
       <header className="ObjectDetection-header">
           <h1>
               Object Detection
           </h1>
-          <p>
-              Detect stuff
-          </p>
       </header>
+
+      <div className='ObjectDetection-body'>
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            errors,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
+              { errors && <div>
+              {errors.maxNumber && <span>Number of selected images exceed maxNumber</span>}
+              {errors.acceptType && <span>Your selected file type is not allow</span>}
+              {errors.maxFileSize && <span>Selected file size exceed maxFileSize</span>}
+              {errors.resolution && <span>Selected file is not match your desired resolution</span>}
+              </div> }
+              {/* {<div {...dragProps}>
+                {isDragging ? "Drop here please" : "Upload space"}
+                {imageList.map((image, index) => (
+                  <img key={index} src={image.data_url} />
+                ))}
+              </div>} */}
+              <button
+                style={isDragging ? { color: 'red' } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop here
+              </button>
+              &nbsp;
+              <button onClick={onImageRemoveAll}>Remove all images</button>
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img src={image['data_url']} alt="" width="100" />
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)}>Update</button>
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </ImageUploading>
+      </div>
     </div>
   );
 }
