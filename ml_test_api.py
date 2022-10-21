c12eac2155c9159f6c089c41f4260dde50929f6d
Ryan Dalby <dalbyryan3@gmail.com>
2022-10-21 15:44:24 -0600

    Added text size adjusting and requirements.txt

--- ml_test_api.py
+++ ml_test_api.py
@@ -10,6 +10,9 @@ api = Api(app)
 detect_thresh = 0.9
 object_detector = ObjectDetector(detect_thresh)
 staged_image = RESTfulImage()
+bb_box_border_width=5
+bb_box_text_font_size=75
+# result_image = RESTfulImage() # A potentially smarter way to do this would be have result image resource and actually have the REST API use itself to put it when a result image is generated, then this result image resource can be queried for the current result
 
 class ObjectDetectorResource(Resource):
     def __init__(self) -> None:
@@ -18,12 +21,12 @@ class ObjectDetectorResource(Resource):
     def get(self):
         img = staged_image.get_PIL()
         model_name = object_detector.get_model_name()
-        res = {'model_name':model_name}
+        res = {'model_name':model_name, 'detect_thresh':detect_thresh}
         if img is None:
             return res
-        object_detector_image, _, _ = object_detector.predict(img)
+        object_detector_image, _, _ = object_detector.predict(img, bb_box_border_width=bb_box_border_width, bb_box_font_size=bb_box_text_font_size)
         object_detector_image = RESTfulImage.encode_PIL_as_b64_str(object_detector_image)
-        res['object_detector_image'] = object_detector_image
+        res['result_image'] = object_detector_image
         return res
 
 class StagedImageResource(Resource):
--- object_detector.py
+++ object_detector.py
@@ -1,11 +1,10 @@
 import numpy as np
-from PIL import Image
-import torch
-import torchvision
-from torchvision.io.image import read_image
+from matplotlib import font_manager
+from PIL import Image, ImageFont
 from torchvision.models.detection import fasterrcnn_mobilenet_v3_large_fpn, FasterRCNN_MobileNet_V3_Large_FPN_Weights
 from torchvision.utils import draw_bounding_boxes
 from torchvision.transforms.functional import to_pil_image, pil_to_tensor
+import os
 
 class ObjectDetector:
     def __init__(self, detect_thresh: float) -> None:
@@ -16,11 +15,15 @@ class ObjectDetector:
 
         # Step 2: Initialize the inference transforms
         self.preprocess = self.weights.transforms()
+
+        # Misc
+        font = font_manager.FontProperties(family='sans-serif', weight='bold')
+        self.font_path = font_manager.findfont(font)
     
     def get_model_name(self)->str:
         return "FasterRCNN_MobileNet_V3_Large_FPN"
 
-    def predict(self, img: Image) -> Image:
+    def predict(self, img: Image, bb_box_font_size=None, bb_box_border_width=None) -> Image:
         # Step 3: Apply inference preprocessing transforms
         img_tensor = pil_to_tensor(img)
         batch = [self.preprocess(img_tensor)]
@@ -28,6 +31,6 @@ class ObjectDetector:
         # Step 4: Use the model and visualize the prediction
         prediction = self.model(batch)[0]
         labels = [self.weights.meta["categories"][i] for i in prediction["labels"]]
-        box = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=labels, colors="red", width=4, font_size=30)
+        box = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=labels, width=bb_box_border_width, colors="red", font=self.font_path, font_size=bb_box_font_size)
         im = to_pil_image(box.detach())
         return im, prediction, labels
